e0f842ebbc26797265717c310b343d370ec9c3f9
arkadij3 <nagabedanarkadij2@gmail.com>
2024-12-08 18:48:20 +0300

    first commit

---
+++ .idea/src/app.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+        data: () => ({
+            items: [1, 2, 3, 4, 5, 6, 7, 8]
+        }),
+        methods: {
+            addItem() {
+                this.items.unshift(this.$refs.myInput.value)
+                this.$refs.myInput.value = ''
+            }
+        },
+        computed: {
+            evenItems() {
+                return this.items.filter(i => i % 2 === 0)
+            }
+        }
+    }
+).mount('#app')
